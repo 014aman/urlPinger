c1e045dbec83f511fffe02378ae1b6178d6f351c
014aman <163138486+014aman@users.noreply.github.com>
2024-08-24 18:24:28 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import UrlPinger from './UrlPinger';
+
+function App() {
+  return (
+    <div className="App">
+      <UrlPinger />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UrlPinger.jsx
@@ -0,0 +1,34 @@
+// src/components/UrlPinger.jsx
+
+import React, { useEffect, useState } from "react";
+
+const UrlPinger = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(`https://thetutors.onrender.com/reload/`)
+        .then((response) => response.json())
+        .then((data) => console.log("Data received:", data))
+        .catch((error) => console.error("Error fetching data:", error));
+    };
+
+    // Fetch data initially
+    fetchData();
+
+    // Set up interval to fetch data every 2 minutes (2 * 60 * 1000 ms)
+    const intervalId = setInterval(fetchData, 2 * 60 * 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h1>URL Pinger</h1>
+      <p>This app hits a URL every 2 minutes.</p>
+      <p>The URL is hit {count} times.</p>
+    </div>
+  );
+};
+
+export default UrlPinger;
